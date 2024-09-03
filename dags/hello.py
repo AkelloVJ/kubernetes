758adaa15acb656443814c8c5fe004e8f8f31ee1
akellovj <akellohvictor@gmail.com>
2024-09-03 15:38:41 +0300

    new2

--- dags/hello.py
+++ dags/hello.py
@@ -3,27 +3,29 @@ from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 
 default_args = {
-    'owner': 'datamasterylab',
+    'owner': 'Akello',
     'start_date': datetime(year=2024, month=1, day=25),
-    'catchup': False
+    'retries': 1,  # Number of retries in case of failure
+    'retry_delay': timedelta(minutes=5),  # Delay between retries
 }
 
 dag = DAG(
     dag_id='hello_world',
     default_args=default_args,
-    schedule_interval=timedelta(days=1)  # Use schedule_interval instead of schedule
+    schedule_interval=timedelta(days=1),  # Schedule interval
+    catchup=False,  # Skip past runs if the DAG was not running
 )
 
 t1 = BashOperator(
     task_id='hello_world',
     bash_command='echo "Hello World"',
-    dag=dag
+    dag=dag,
 )
 
 t2 = BashOperator(
     task_id='hello_dml',
     bash_command='echo "Hello Data Mastery Lab"',
-    dag=dag
+    dag=dag,
 )
 
 t1 >> t2
